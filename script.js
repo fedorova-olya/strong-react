f7f52f22b53fdbd0d3d6a1be92c6d1ae2f7dc332
Olya <fedorovaolya0410@gmail.com>
2020-06-15 13:57:39 +0300

    practic fetch

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-const getResource = async (url) => {
-  const res = await fetch(url),
-    some = await res.json();
-  return some;
+class getService {
+  constructor() {
+    this._apiBase = 'https://www.anapioficeandfire.com/api';
+}
+getResource = async (url) => {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+  if(!res.ok) {
+    throw new Error(`fetch ${url} error, status ${res.status}`)
+  }
+
+    return await res.json();
+  };
+  getAllCharacters() {
+    return this.getResource(`/characters?page=5&pageSize=10`)
+  }
+  getCharacter(id) {
+    return this.getResource(`/characters/${id}`)
+  }
 }
 
-getResource('https://jsonplaceholder.typicode.com/posts/1')
-  .then((json) => console.log('Success', json))
-  .catch(error => console.error('Error', error))
+const got = new getService();
+
+got.getAllCharacters()
+  .then(res => {
+    res.forEach(el => console.log(el.name))
+  });
+
+got.getCharacter(269)
+  .then(res => console.log(res));
